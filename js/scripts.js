24e96604bcf26f826527b418637ff29708564b9d
Carson Canales and David Cuthell <carson@>
2017-07-06 11:36:26 -0700

    add comment to prepend function to make format  understandable for future reference.

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,20 @@ $(document).ready(function(){
     event.preventDefault();
   });
 });
+
+// This is the code, formatted nicely
+//
+// $("div#addressRow").prepend('
+//   <div class="panel panel-default">
+//     <div class="panel-heading">'
+//       + thisName + '
+//     </div>
+//     <div class="panel-body">
+//      <ul>
+//       <li>Address: ' + thisAddress +'</li>
+//       <li>Email: ' + thisEmail + '</li>
+//       <li>Phone Number: ' + thisNumber + '</li>
+//      </ul>
+//     </div>
+//   </div>');
+//
