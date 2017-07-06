0f656ec12fc0d99988951726a773ae457bad061b
Carson Canales and David Cuthell <carson@>
2017-07-06 15:27:09 -0700

    add toggle function to address lists.

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,13 @@ $(document).ready(function(){
     var thisNumber = $("input#number").val();
     $("div#addressRow").prepend('<div class="panel panel-default"><div class="panel-heading">' + thisName + '</div><div class="panel-body"><ul><li>Address: ' + thisAddress +'</li><li>Email: ' + thisEmail + '</li><li>Phone Number: ' + thisNumber + '</li></ul></div></div>');
 
+    $(".panel-default").first().click(function(){
+      $(this).find(".panel-body").toggle();
+    });
+
     event.preventDefault();
   });
+
 });
 
 // This is the code, formatted nicely
