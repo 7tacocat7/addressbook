f226b75a405b2e77c55a2890c16fd1e63f431924
Carson Canales and David Cuthell <carson@>
2017-07-06 11:29:50 -0700

    add panel box creation with new contact info

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#myForm").submit(function(event){
+    var thisName = $("input#name").val();
+    var thisAddress = $("input#address").val();
+    var thisEmail = $("input#email").val();
+    var thisNumber = $("input#number").val();
+    $("div#addressRow").prepend('<div class="panel panel-default"><div class="panel-heading">' + thisName + '</div><div class="panel-body"><ul><li>Address: ' + thisAddress +'</li><li>Email: ' + thisEmail + '</li><li>Phone Number: ' + thisNumber + '</li></ul></div></div>');
+
+    event.preventDefault();
+  });
+});
